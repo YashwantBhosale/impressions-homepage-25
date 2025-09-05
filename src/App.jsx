d7971d0500fa3cec51cc286a606b830d15ae6617
YashwantBhosale <yashwantbhosale07@gmail.com>
2025-09-05 10:20:35 +0530

    init: Impressions 2025 homepage

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Welcome to Impressions 2025!</h1>
+        <p>coming soon ...</p>
+      </div>
+    </>
+  )
+}
+
+export default App
